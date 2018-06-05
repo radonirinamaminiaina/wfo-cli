f1b3d602ddf066ea6b54c1f0682b2d70ed5abf66
Radonirina <rado93devweb@gmail.com>
2018-06-05 13:36:56 +0300

    ignore node_modules

--- src/action.js
+++ src/action.js
@@ -6,9 +6,18 @@ const action = {
 		const findNewCommand = arg.findIndex((element) => element === 'new')
 		if (findNewCommand !== -1) {
 			console.log('generate your template ...')
-			const src = path.resolve(path.dirname(require.main.filename))
+			const src = path.resolve(path.dirname(require.main.filename) + '/templates')
 			const dest = path.resolve('./')
-			console.log(src, dest)
+			console.log(dest)
+			const filter = (src, dest) => {
+				console.log(src.match(/node_modules/))
+				if (!src.match(/node_modules/)) return true
+			}
+
+			fs.copy(src, dest, {filter: filter}, err => {
+				if (err) return console.err(err)
+				console.log('success')
+			})
 		} else {
 			console.log('not found')
 		}
