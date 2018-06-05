f6504eba09cfbb29197c2375db2a7fcd87beb30c
Radonirina <rado93devweb@gmail.com>
2018-06-05 14:27:48 +0300

    add comments

--- src/action.js
+++ src/action.js
@@ -7,6 +7,7 @@ const log = console.log
 
 const action = {
 	copyTemplate: (arg) => {
+		// find index of arg
 		const findNewCommand = arg.findIndex((element) => element === 'new')
 		if (findNewCommand !== -1) {
 			log(chalk.green('Generate your template ...'))
@@ -16,15 +17,17 @@ const action = {
 				log(`Copy ${src} to ${dest}`)
 				if (!src.match(/node_modules/)) return true
 			}
-
+			// copy our template
 			fs.copy(src, dest, {filter: filter}, err => {
 				if (err) return console.err(err)
 				log(chalk.blue.bold('Your template is ready. \n\n'))
 				log(chalk.green('Initialize git ...'))
+				// initialize git repo
 				exec('git init && git add . && git commit -am "initialized by fo generator"', (err, stdout) => {
 					if (err) return console.error(err)
 					log(chalk.blue.bold('git initialized.'))
 					log(chalk.green('Installing your module ...'))
+					// install module
 					exec('npm install', (err, stdout) => {
 						if (err) return console.error(err)
 						log(chalk.blue.bold('Module installed.'))
@@ -37,7 +40,7 @@ const action = {
 				})
 			})
 		} else {
-			log('not found')
+			log('errcom: command not found. \n Use "fo --help"')
 		}
 	}
 }
